f4b4ac6810fd9255b752253c7c9a4239da9acdf6
Jonathan Perlow <jonathan.perlow@iths.se>
2023-11-12 14:01:07 +0100

    Added a class library PublisherData with a class PublisherContext and installed Entity framework core with sql server.

---
+++ PublisherData/PublisherContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PublisherDomain;
+
+namespace PublisherData;
+public class PublisherContext : DbContext
+{
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Book> Books { get; set; }
+}
