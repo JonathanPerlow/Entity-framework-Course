f954167df5f5562aaf279125eda5bf9c1cb49044
Jonathan Perlow <jonathan.perlow@iths.se>
2023-11-12 15:46:33 +0100

    Added Authors and books to the context. Ovverided the onconfiguring method to use a hardcodded connection string to a sql database.

--- PublisherData/PublisherContext.cs
+++ PublisherData/PublisherContext.cs
@@ -6,4 +6,12 @@ public class PublisherContext : DbContext
 {
     public DbSet<Author> Authors { get; set; }
     public DbSet<Book> Books { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // Hard codding the database connectionstring for the demo. Will inject it with asp.net later in the run time.
+        optionsBuilder.UseSqlServer(
+            "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = PubDatabase"
+            );
+    }
 }
